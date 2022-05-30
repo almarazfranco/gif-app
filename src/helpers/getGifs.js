3d363d4366b724d771e20ac2601cdfacf68a7b35
Franco Almaraz <almarazfranco.zx@gmail.com>
2022-05-30 01:37:17 -0300

    url api change http for https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@ const getGifs = async (category) => {
 
   try {
 
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=54mJifc3nvupFr99mg9FytBiLHqRawZ3&q=${encodeURI(category)}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=54mJifc3nvupFr99mg9FytBiLHqRawZ3&q=${encodeURI(category)}&limit=20`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     const gifs = data.map( element => {
